53c7aeba9264dd31df6661177e349d8dc17a81c7
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-01-03 02:09:21 +1100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,154 @@
 //Copyright George Wagenknecht - 2022 - All rights reserved.
-// FTL information transfer prototype code(aim to use more complex math, preferably one not quickly checkable, however this code can exchange a state from each port)
+//FTL information transfer for quantum computer, prototype code.
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
+int portA(int X, int Y)//Sync X & Y to time
+{
+    int data = 0;//bit to send
+    vector<int> ratios = { 1,2,3,4,5};
+    //state:  A + B = C
+    //state 1 = 1,2,3 & state 3 =  3,6,9 works //iterate this
+    int A = 1;
+    int C = 3; // result, shared by both ports
+    if(data == 0)
+    {
+        if(Y == 0)
+        {
+            A = 1;
+            C = 3; // result, shared by both ports
+        }
+        if(Y == 1)
+        {
+            A = 3;
+            C = 9; // result, shared by both ports
+        }
+    }
+    if(data == 1)
+    {
+        if(Y == 1)
+        {
+            A = 1;
+            C = 3; // result, shared by both ports
+        }
+        if(Y == 0)
+        {
+            A = 3;
+            C = 9; // result, shared by both ports
+        }
+    }
+    if(A*ratios[X] == C - A)
+    {
+        return (A*ratios[X]);
+        //return state 1, state 2 or state 3
+    }
+    return 0;
+}
+int portB(int X, int Y)//Sync X & Y to time
+{
+    int data = 1;//bit to send
+    vector<int> ratios = { 1,2,3,4,5};
+    //state:  A + B = C
+    //state 1 = 1,2,3 & state 3 =  3,6,9 works //iterate this
+    int B = 2;
+    int C = 3; // result, shared by both ports
+    if(data == 0)
+    {
+        if(Y == 0)
+        {
+            B = 2;
+            C = 3; // result, shared by both ports
+        }
+        if(Y == 1)
+        {
+            B = 6;
+            C = 9; // result, shared by both ports
+        }
+    }
+    if(data == 1)
+    {
+        if(Y == 1)
+        {
+            B = 2;
+            C = 3; // result, shared by both ports
+        }
+        if(Y == 0)
+        {
+            B = 6;
+            C = 9; // result, shared by both ports
+        }
+    }
+    if(B/ratios[X] == C - B)
+    {
+        return (B/ratios[X]);
+        //return state 1, state 2 or state 3
+    }
+    return 0;
+}
 int main()
 {
-    //state:  A + B = C
-    //state 1 = 1,2,3 & state 2 =  2,2,4 & state 3 =  3,6,9 works //iterate this
-    int A = 3; // port A
-    int B = 6; // port B
-    int C = 9; // result, shared by both ports
-    vector<int> ratios = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};//use float for more precision
-    for(int X = 0; X < ratios.size(); X++) //distribute code between ports and sync X, try to guess the state each port is in by bruteforce
+    vector<int> ratios = { 1,2,3,4,5};//use float for more precision
+    bool entanglementBlocked = false;
+    bool exit = false;
+    for(int Partition = 0; Partition < 2 && exit == false; Partition++)
     {
-        if(A*ratios[X] == C - A && B/ratios[X] == C - B) //ANDing represents entangled port A & port B, divide each statement and 100% blockage of wavefunction indicates successful transfer
+        for(int T = 0; T < ratios.size() && exit == false; T++)
         {
-            //port A = if(A*ratios[X] == C - A && Entanglement == true)
-            //port B = if(B/ratios[X] == C - B && Entanglement == true)
-            //continue
-            cout << "Original" << endl;
-            cout << "Node A = " << A << " Node B = " << B << endl;
-            cout << "Sent!" << endl;
-            cout << "Node A = " << (A*ratios[X]) << " Node B = " << (B/ratios[X]) << endl;
-            //return state 1, state 2 or state 3
+            int A = 1;
+            int B = 2;
+            int C = 3;
+            if(Partition == 0)
+            {
+                A = 1;
+                B = 2;
+                C = 3;
+            }
+            if(Partition == 1)
+            {
+                A = 3;
+                B = 6;
+                C = 9;
+            }
+            int teleportedToAlpha = portB(T,Partition);//physical process
+            int teleportedToBeta = portA(T,Partition);//physical process
+            cout << "Cycle: " << T << ": " << teleportedToAlpha << " " << teleportedToBeta;
+            if(teleportedToAlpha == 1 && teleportedToBeta == 2 && Partition == 0)
+            {
+                cout << " Sent off!" << endl;//done
+            }
+            if(teleportedToAlpha == 1 && teleportedToBeta == 6 && Partition == 0)
+            {
+                cout << " Sent on!" << endl;//done
+            }
+            if(teleportedToAlpha == 3 && teleportedToBeta == 2 && Partition == 0)
+            {
+                cout << " Sent off!" << endl;//done
+            }
+            if(teleportedToAlpha == 3 && teleportedToBeta == 6 && Partition == 0)
+            {
+                cout << " Sent on!" << endl;//done
+            }
+            if(teleportedToAlpha == 1 && teleportedToBeta == 2 && Partition == 1)
+            {
+                cout << " Sent on!" << endl;//done
+            }
+            if(teleportedToAlpha == 1 && teleportedToBeta == 6 && Partition == 1)
+            {
+                cout << " Sent on!" << endl;//done
+            }
+            if(teleportedToAlpha == 3 && teleportedToBeta == 2 && Partition == 1)
+            {
+                cout << " Sent off!" << endl;//done
+            }
+            if(teleportedToAlpha == 3 && teleportedToBeta == 6 && Partition == 1)
+            {
+                cout << " Sent off!" << endl;//done
+            }
+            if(teleportedToAlpha == 0 || teleportedToBeta == 0)
+            {
+                cout << endl;
+            }
         }
     }
     return 0;
