982c2de98a107bc33d476128f2e74589644f7143
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-01-03 15:39:16 +1100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -205,7 +205,7 @@ int main()//server
         }
         cout << endl;
         //communication is naturally 10,000 times faster due to speed of quantum entanglement(once memory and logic is in portA and portB)
-        //by allowing the light valve to operate using a control qubit, the activity can effectively link qubits in into a logical qubit.
+        //by allowing the light valve to operate using a control qubit, the activity can effectively link qubits in into a logical qubit, each performing logical operations to each non-control qubit(instantly).
     }
     return 0;
 }
