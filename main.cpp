df37deed079679055eb15a13324d3d856e295bc7
gw8080 <52276914+gw8080@users.noreply.github.com>
2022-01-03 16:32:38 +1100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-/* FTL information transfer for quantum computer, prototype code.
+/* code for quantum computer, prototype code.
     Copyright (C) 2022 George Wagenknecht
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
